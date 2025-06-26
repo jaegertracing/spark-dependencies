094ad449381b736888ac84bc0df41d4affef5601
Louis Genasi <112476169+lgenasi-gocity@users.noreply.github.com>
2025-06-26 08:48:25 +0100

    Update SpanDeserializer.java
    Signed-off-by: Louis Genasi <112476169+lgenasi-gocity@users.noreply.github.com>

--- jaeger-spark-dependencies-elasticsearch/src/main/java/io/jaegertracing/spark/dependencies/elastic/json/SpanDeserializer.java
+++ jaeger-spark-dependencies-elasticsearch/src/main/java/io/jaegertracing/spark/dependencies/elastic/json/SpanDeserializer.java
@@ -97,7 +97,7 @@ public class SpanDeserializer extends StdDeserializer<Span> {
     }
 
     JsonNode referencesNode = node.get("references");
-    if (referencesNode != null && !referencesNode.isNull()) {
+    if (referencesNode != null) {
         Reference[] referencesArr = objectMapper.treeToValue(referencesNode, Reference[].class);
         references.addAll(Arrays.asList(referencesArr));
     }
