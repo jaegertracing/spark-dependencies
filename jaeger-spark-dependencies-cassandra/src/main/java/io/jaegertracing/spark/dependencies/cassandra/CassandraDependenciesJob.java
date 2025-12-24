414ff61ae7621f406c4d9dc788cf9141ec483b50
Yuri Shkuro <yurishkuro@users.noreply.github.com>
2025-12-23 20:17:02 -0500

    Replace license header with SPDX reference and remove copyright year (#184)

--- jaeger-spark-dependencies-cassandra/src/main/java/io/jaegertracing/spark/dependencies/cassandra/CassandraDependenciesJob.java
+++ jaeger-spark-dependencies-cassandra/src/main/java/io/jaegertracing/spark/dependencies/cassandra/CassandraDependenciesJob.java
@@ -1,16 +1,7 @@
 /**
- * Copyright 2017 The Jaeger Authors
+ * Copyright (c) The Jaeger Authors
  * Copyright 2016-2017 The OpenZipkin Authors
- *
- * Licensed under the Apache License, Version 2.0 (the "License"); you may not use this file except
- * in compliance with the License. You may obtain a copy of the License at
- *
- * http://www.apache.org/licenses/LICENSE-2.0
- *
- * Unless required by applicable law or agreed to in writing, software distributed under the License
- * is distributed on an "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express
- * or implied. See the License for the specific language governing permissions and limitations under
- * the License.
+ * SPDX-License-Identifier: Apache-2.0
  */
 package io.jaegertracing.spark.dependencies.cassandra;
 
@@ -18,8 +9,6 @@ import static com.datastax.spark.connector.japi.CassandraJavaUtil.javaFunctions;
 import static com.datastax.spark.connector.japi.CassandraJavaUtil.mapRowTo;
 import static com.datastax.spark.connector.japi.CassandraJavaUtil.mapToRow;
 
-import com.datastax.spark.connector.cql.CassandraConnector;
-import com.datastax.spark.connector.japi.CassandraRow;
 import com.google.common.base.Joiner;
 import com.google.common.net.HostAndPort;
 import io.jaegertracing.spark.dependencies.DependenciesSparkHelper;
@@ -33,7 +22,6 @@ import java.time.Period;
 import java.time.ZoneOffset;
 import java.time.ZonedDateTime;
 import java.time.temporal.ChronoUnit;
-import java.time.temporal.TemporalUnit;
 import java.util.Collections;
 import java.util.HashSet;
 import java.util.LinkedHashMap;
